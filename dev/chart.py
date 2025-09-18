4274d28344256454f02521e108746af5088ad70e
Rob Liston <rliston@gmail.com>
2025-09-18 15:41:46 -0700

    added sample log file added MIT license

--- dev/chart.py
+++ dev/chart.py
@@ -1,4 +1,23 @@
-#step            0 train_loss 4.1681 val_loss 4.1682 sample NlasVZbSlXFQJzqjPmHmV.EkJ eEhLa?.Cu.eUPOeK&A. DMIkvbPXzQuDx&SohayA-.CdhaSEHiUWp-akH,pIYLvxpuHpAPgqT'ihQcVB?d-nb-v?-OzO3dtJSCiVsS-zyxA&JxxeXV!u.ErkK
+# Copyright (c) 2024 Silicon Perception Inc (www.siliconperception.com)
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import argparse
 import numpy as np ; print('numpy ' + np.__version__)
 import matplotlib.pyplot as plt
